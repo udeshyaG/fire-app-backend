6374eed8e592cd32b94e86e9869db5a459ce6be2
Udeshya <udeshyagiri@gmail.com>
2021-06-03 14:35:51 +0530

    change

--- db/migrations/20210603081022_add_image_url_to_complaints.js
+++ db/migrations/20210603081022_add_image_url_to_complaints.js
@@ -1,13 +1,13 @@
 // const tableNames = require('../../src/constants/table-names');
 
-// exports.up = async (knex) => {
-//   await knex.schema.table(tableNames.userComplaints, function (table) {
-//     table.string('image_url');
-//   });
-// };
+exports.up = async (knex) => {
+  // await knex.schema.table(tableNames.userComplaints, function (table) {
+  //   table.string('image_url');
+  // });
+};
 
-// exports.down = async (knex) => {
-//   await knex.schema.table(tableNames.userComplaints, function (table) {
-//     table.dropColumn('image_url');
-//   });
-// };
+exports.down = async (knex) => {
+  // await knex.schema.table(tableNames.userComplaints, function (table) {
+  //   table.dropColumn('image_url');
+  // });
+};
